0fc665bc1b9e5a449695ffd79b82d4bb61a606f2
Luis <a20153229@pucp.pe>
2019-11-29 11:02:45 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  #get 'sessions/new'
+  resources :sessions
 
   resources :students
   resources :item_tools
